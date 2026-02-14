7535037d59e2bba37893e7288f387c6c7ff8c043
Sandeepsahu18 <editingking998@gmail.com>
2026-02-15 00:17:56 +0530

    resume part done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import "./app.scss";
 import { Dock } from "./components/Dock";
 import Navbar from "./components/Navbar";
 import Github from "./components/windows/Github";
+import Note from "./components/windows/Note";
+import Resume from "./components/windows/Resume";
 const App = () => {
   return (
     <main>
       <Navbar />
       <Dock />
       <Github/>
+      <Note/>
+      <Resume/>
     </main>
   );
 };
